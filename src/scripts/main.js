7b1ac3834778ab80358f01dbb23a7ee7f7e9ff7f
Н  е андрей <oleksandrlib4@gmail.com>
2025-07-17 15:08:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,15 +7,18 @@ document.querySelectorAll('.tree li').forEach((li) => {
     return;
   }
 
-  const text = li.firstChild.textContent.trim();
+  const textNode = Array.from(li.childNodes).find(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+  );
 
   const span = document.createElement('span');
 
-  span.textContent = text;
+  span.textContent = textNode.textContent.trim();
   span.style.cursor = 'pointer';
 
-  li.firstChild.textContent = '';
-  li.insertBefore(span, childUl);
+  li.insertBefore(span, textNode);
+  li.removeChild(textNode);
 
   span.addEventListener('click', () => {
     childUl.hidden = !childUl.hidden;
